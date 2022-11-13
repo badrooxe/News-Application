55108beb9ee4e46b3fb234416d5dfccb7f5c4c67
badrooxe <badrooxe55@gmail.com>
2022-11-13 21:01:00 +0100

    first

---
+++ app/src/main/java/com/example/newsproject/ItemActivity.java
@@ -0,0 +1,49 @@
+package com.example.newsproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar loader;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.opened_item);
+
+        Intent intent=getIntent();
+        url = intent.getStringExtra("link");
+        loader = findViewById(R.id.loader);
+        webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                loader.setVisibility(View.VISIBLE);
+                view.loadUrl(url);
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                loader.setVisibility(View.GONE);
+            }
+        });
+
+        webView.loadUrl(url);
+
+    }
+}
---
+++ app/src/main/java/com/example/newsproject/List_item.java
@@ -0,0 +1,50 @@
+package com.example.newsproject;
+
+public class List_item {
+
+    String title;
+    String link;
+    String category;
+    String description;
+    String Img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return Img;
+    }
+
+    public void setImg(String img) {
+        Img = img;
+    }
+}
